ac1e2279d2c0ebbc791837ec0fa7be7a1c37d516
Vinicius Rodrigues <viniciuslr@gmail.com>
2011-09-19 14:57:25 -0300

    updating SpaceSaverTest

--- tests/src/tests/SpaceSaverTest.cpp
+++ tests/src/tests/SpaceSaverTest.cpp
@@ -26,13 +26,21 @@
 
 class SpaceSaverTest : public ERMSpace {};
 
-TEST_F( SpaceSaverTest, simpleTest )
+TEST_F( SpaceSaverTest, testNewFileSetup )
 {
+	int modelVersion = 1;
 
 	co::RefPtr<co::IObject> spaceObj = co::newInstance( "ca.Space" );
 	ca::ISpace* space = spaceObj->getService<ca::ISpace>();
-
+	
 	startWithExtendedERM();
+
+	spaceObj->getService("universe");
+
+	ca::IUniverse* universe = static_cast<ca::IUniverse*>( spaceObj->getService( "universe" ) );
+	assert( universe );
+
+	ca::IModel* model = static_cast<ca::IModel*>( universe->getProvider()->getService( "model" ) );
 
 	co::RefPtr<co::IObject> universeObj = co::newInstance( "ca.Universe" );
 	ca::IUniverse* universe = universeObj->getService<ca::IUniverse>();
@@ -45,12 +53,59 @@ TEST_F( SpaceSaverTest, simpleTest )
 	co::RefPtr<co::IObject> obj = co::newInstance( "ca.SpaceSaverSQLite3" );
 	co::RefPtr<ca::ISpaceSaver> spaceSav = obj->getService<ca::ISpaceSaver>();
 
+	std::string fileName = "SimpleSpaceSave.db";
+	remove( fileName.c_str() );
+
 	ca::INamed* file = (obj->getService<ca::INamed>());
-	file->setName( "SimpleSpaceSave.db" );
+	file->setName( fileName );
 	
 	spaceSav->setModel( _model.get() );
-	spaceSav->setModelVersion(1);
+	spaceSav->setModelVersion(modelVersion);
 	spaceSav->setSpace( space );
-	ASSERT_NO_THROW(spaceSav->setup());
 
+	EXPECT_NO_THROW(spaceSav->setup());
+	
+	FILE* dbFile = 0;
+	EXPECT_NO_THROW(dbFile = fopen(fileName.c_str(), "r"));
+	EXPECT_TRUE(dbFile != 0);
+
+	sqlite3* db; 
+	sqlite3_open( fileName.c_str(), &db );
+
+	sqlite3_stmt *statement;
+
+	sqlite3_prepare_v2(db, "SELECT name FROM sqlite_master WHERE type = 'table' and name <> 'sqlite_sequence' ORDER BY name", -1, &statement, 0);
+
+	char* tables[] = { "CALCIUM_MODEL",
+					    "ENTITY",
+						"FIELD",
+						"FIELD_VALUES",
+						"OBJECT"
+					 };
+
+	int i = 0;
+	while (sqlite3_step(statement) == SQLITE_ROW)
+	{
+		EXPECT_TRUE(strcmp((char*)sqlite3_column_text(statement, 0), tables[i])==0) ;
+		i++;
+	}
+	EXPECT_EQ( i, 5);
+
+	sqlite3_prepare_v2(db, "SELECT * FROM CALCIUM_MODEL", -1, &statement, 0);
+
+	int cols = sqlite3_column_count(statement);
+
+	sqlite3_step(statement);
+	
+	int caModelId = atoi((char*)sqlite3_column_text(statement, 0));
+
+	EXPECT_EQ(caModelId, 1);
+
+	//Model name
+	EXPECT_TRUE(strcmp((char*)sqlite3_column_text(statement, 1), _model->getName().c_str()) == 0);
+
+	int caModelVersion = atoi((char*)sqlite3_column_text(statement, 0));
+	EXPECT_EQ(caModelVersion, 1);
+
+	sqlite3_close(db);
 }
